b9e38f60b5b3db6b9a01283d16b9416df05e4759
AkidoLD <alexrayan14231423@gmail.com>
2025-06-19 10:55:35 +0100

    break : Go resoudre le bug de wordbase lorsque la base est vide ou inexistante

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,18 @@
 #include "ui/MainWindow.hpp"
 #include "controls/AppControls.hpp"
 #include "model/GenyRand.hpp"
-#include <QListWidget>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
     QApplication app (argc, argv);
-    GenyRand genyModel("/home/akido-ld/Documents/Memo/Guide pour Github et Git.txt");
+    GenyRand genyModel("data/wordbase.dat");
     MainWindow genyView("Geny-int");
     //
     AppControls genyControl(&genyView, &genyModel);
     genyView.setWindowTitle("Geny");
     genyView.resize(720, 940);
-    genyView.show();
-    QListWidget list;
-    
+    genyView.show();    
     //
     return app.exec();
 }
